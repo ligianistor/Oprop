0e9dd1692ca1c7a31aa08cb9ff64b443a7fbf927
Ligia Nistor <ligia.nicoleta@gmail.com>
2015-04-29 18:50:47 -0400

    added more annotations Composite.java

--- src/testcases/cager/jexpr/Composite.java
+++ src/testcases/cager/jexpr/Composite.java
@@ -1,159 +1,192 @@
 package testcases.cager.jexpr;
 
 class Composite {
-	int count;
-	Composite left;
-	Composite right;
-	Composite parent;	  
+int count;
+Composite left;
+Composite right;
+Composite parent;	  
 	
-	predicate count(int c) = 
-			exists Composite ol, Composite or,
-			int lc, int rc : 
-			this.count -> c && c == lc + rc + 1  
-			&& (this#0.5 left(ol, lc)) 
-			&& (this#0.5 right(or, rc)) 
+predicate count(int c) = 
+		exists Composite ol, Composite or,
+		int lc, int rc : 
+		this.count -> c && c == lc + rc + 1  
+		&& (this#0.5 left(ol, lc)) 
+		&& (this#0.5 right(or, rc)) 
 			
-	predicate left(Composite ol, int lc) =
-		this.left -> ol &&
-		((ol== null ~=> (lc == 0) ) ||
-		(ol != null ~=> (ol#0.5 count(lc)))
-		)
+predicate left(Composite ol, int lc) =
+	this.left -> ol &&
+	((ol== null ~=> (lc == 0) ) ||
+	(ol != null ~=> (ol#0.5 count(lc)))
+	)
 		
-	predicate right(Composite or, int rc) =
-		this.right -> or &&
-		((or== null ~=> (rc == 0) ) ||
-		(or != null ~=> (or#0.5 count(rc)))
-		)
+predicate right(Composite or, int rc) =
+	this.right -> or &&
+	((or== null ~=> (rc == 0) ) ||
+	(or != null ~=> (or#0.5 count(rc)))
+	)
 	
-	predicate parent() =
-		exists Composite op, int c, double k:
-		this.parent -> op &&
-		(this#0.5 count(c)) &&
-		( (op != null ~=> (op#k parent())) &&
-		  ( (op#0.5 left(this, c)) ||
-		    (op#0.5 right(this, c))
-		   ) &&
-		  (op==null ~=> (this#0.5 count(c)))
-		)
+predicate parent() =
+	exists Composite op, int c, double k:
+	this.parent -> op &&
+	(this#0.5 count(c)) &&
+	( (op != null ~=> (op#k parent())) &&
+		 ( (op#0.5 left(this, c)) ||
+		   (op#0.5 right(this, c))
+		  ) &&
+		 (op==null ~=> (this#0.5 count(c)))
+	)
 		
 		
-	void Composite() 
-	ensures (this#0.5 parent()) &&
-		(this#0.5 left(null,0)) &&
-		(this#0.5 right(null, 0))	
-	{
-		this.count = 1;
-		this.left = null;
-		this.right = null;
-		this.parent = null; 
-		pack(this#1 right(null, 0));
-		pack(this#1 left(null, 0));
-		splitFrac(this#1 right(null, 0), 2);
-		splitFrac(this#1 left(null, 0), 2);
-		pack(this#1 count(0));
-		splitFrac(this#1 count(0), 2);
-		pack(this#1 parent());
-		splitFrac(this#1 parent(), 2);	
-	}
+void Composite() 
+ensures (this#0.5 parent()) &&
+	(this#0.5 left(null,0)) &&
+	(this#0.5 right(null, 0))	
+{
+	this.count = 1;
+	this.left = null;
+	this.right = null;
+	this.parent = null; 
+}
 
-	void updateCountRec() 
-	~ double k1, double k, double k2:
-	Composite opp, int lcc,
-	Composite or, Composite ol,
-	int lc, int rc:
-	requires unpacked(this#k1 parent()) &&
-		this.parent -> opp &&
-		(opp != this) &&
-	  (((( (opp != null) ~=> (opp#k parent()) &&
-	     ((opp#0.5 left(this, lcc)) || (opp#0.5 right(this, lcc))
-	     )) || 
-	     ((opp == null) ~=> (this#0.5 count(lcc)))
-	     )))  &&
-	   unpacked(this#0.5 count(lcc)) &&
-	   (this#0.5 left(ol, lc)) &&
-	   (this#0.5 right(or, rc))
-	ensures (this#k2 parent())
-	{
-			
-	if (this.parent != null) {
+//This version assumes this is the right child of opp.
+//We need a very similar version of updateCountRec, where
+//we assume this is the left child of opp.
+void updateCountRec() 
+~ double k1, double k, double k2:
+Composite opp, int lcc,
+Composite or, Composite ol,
+int lc, int rc:
+requires unpacked(this#k1 parent()) &&
+	this.parent -> opp &&
+	(opp != this) &&
+  (((( (opp != null) ~=> (opp#k parent()) &&
+     ((opp#0.5 left(this, lcc)) || (opp#0.5 right(this, lcc))
+     )) || 
+     ((opp == null) ~=> (this#0.5 count(lcc)))
+     )))  &&
+   unpacked(this#0.5 count(lcc)) &&
+   (this#0.5 left(ol, lc)) &&
+   (this#0.5 right(or, rc))
+ensures (this#k2 parent())
+{
+//We already have access to this.parent from the precondition of 
+//this function.
+if (this.parent != null) {
+	splitFrac(opp#k parent(), 2);
+	unpack(opp#k/2 parent());
+	//We get opp#1/2 count(lccc) from unpacking opp in parent()
+	int lccc;
+	unpack(opp#1/2 count(lccc));
+	//These variables are for existentially quantified variables inside the 
+	//count predicate that we just unpacked.
+	int rrc;
+	Composite orr;
+	//Assume this is the right child of opp.
+	assume (right[opp] == this);
+	//These instantiates are because the programmer knows what to
+	//instantiate to what.
+	//Instantiate the existentially quantified variable to this.
+	//Maybe we do not need these instantiate()
+	instantiate(orr, this);
+	instantiate(rrc, lcc);
+	//The rule in the formal system should be that
+	//if we have two object propositions with different parameters
+	//that are both packed, then the parameters should be the same.
+	addFrac(opp#0.5 right(this, lcc), opp#0.5 right(orr, rrc));
+	//Explain why do we need the full fraction!!!
+	unpack(opp#1.0 right(this, lcc));
+	pack(this#0.5 count(lcc));
+	addFrac(this#0.5 count(lcc), this#0.5 count(lcc));
+	unpack(this#1.0 count(lcc));
+	
+	this.updateCount();
+	pack(opp#1.0 right(this, lcc));
 		
-		this.updateCount();
+	this.parent.updateCountRec();
+	k3:real;
+	pack(opp#k3 parent());
+	pack(this#k2 parent());
 		
-		this.parent.updateCountRec();
-		
-	 } else {
-		 
-		 this.updateCount();
-		 
-	 }
+ } else {
+	addFrac(this#0.5 count(lcc), unpacked(this#0.5 count(lcc)));
+	//After this addFrac, we know that this is unpacked in count()
+	 this.updateCount();
+	splitFrac(unpacked(this#1.0 count(lcc)), 2);
+	pack(this#k2 parent());
 	}
+}
 
-	void updateCount() 
-	int c, int c1, int c2, int newc,
-	Composite ol, Composite or:	
-	requires unpacked(this#1.0 count(c)) &&
-		(this#0.5 left(ol, c1)) &&
-		(this#0.5 right(or, c2))
-	ensures (this#1.0 count(newc))
-	{
+void updateCount() 
+int c, int c1, int c2, int newc,
+Composite ol, Composite or:	
+requires unpacked(this#1.0 count(c)) &&
+	(this#0.5 left(ol, c1)) &&
+	(this#0.5 right(or, c2))
+ensures (this#1.0 count(newc))
+{
 	
-		int newc = 1;
-	
-	if (this.left != null) {
-	 
-		newc = newc + left.count;
-	
-	}
-	
-	if (this.right != null) {
-	 
-		newc = newc + right.count;
-	
-	}
-	
-	this.count = newc; 
-	
-	}
+int newc = 1;
+unpack(this#0.5 left(ol, c1));
+if (this.left != null) {
+	unpack(ol#0.5 count(c1));	 
+	newc = newc + left.count;
+	pack(ol#0.5 count(c1));
+}
+pack(this#0.5 left(ol, c1));
 
-	void setLeft(Composite l) 
-	~ double k1, double k2, double k:
-	requires (this != l) &&
-		(this#0.5 left(null, 0)) &&
-		( (this#k1 parent()) ||
-		 (l#k2 parent())	
-		)
-	ensures (this#k parent())
-	{
-		if (l.parent==null) {
-			
-			l.parent= this;
-			
-			this.left = l;
-			
-			this.updateCountRec(); 
-		
-		}
-	}
+unpack(this#0.5 right(or, c2));	
+if (this.right != null) {
+	unpack(or#0.5 count(c2));
+	newc = newc + right.count;
+	pack(or#0.5 count(c2));
+}
+pack(this#0.5 right(or, c2));		
+this.count = newc; 
+pack(this#1.0 count(newc));
+}
 
-	void setRight(Composite r) 
-	~ double k1, double k2, double k:
-	requires (this != r) &&
-		(this#0.5 right(null, 0)) &&
-		( (this#k1 parent()) ||
-		 (r#k2 parent())	
-		)
-	ensures (this#k parent())
-	{
+void setLeft(Composite l) 
+~ double k1, double k2, double k:
+requires (this != l) &&
+	(this#0.5 left(null, 0)) &&
+	( (this#k1 parent()) ||
+	 (l#k2 parent())	
+	)
+ensures (this#k parent())
+{
+unpack(l#k2 parent());
+if (l.parent==null) {
+			
+	l.parent= this;
+			
+	this.left = l;
+			
+	this.updateCountRec(); 
 		
-		if (r.parent==null) {
-			
-			r.parent = this;
-			
-			this.right = r;
-			
-			this.updateCountRec(); 
-		
-		}
 	}
+}
+
+//We do not need to prove this,
+//it is analogous to setLeft
+void setRight(Composite r) 
+~ double k1, double k2, double k:
+requires (this != r) &&
+	(this#0.5 right(null, 0)) &&
+	( (this#k1 parent()) ||
+	 (r#k2 parent())	
+	)
+ensures (this#k parent())
+{
+		
+if (r.parent==null) {
+			
+	r.parent = this;
+			
+	this.right = r;
+			
+	this.updateCountRec(); 
+		
+	}
+}
 
 }
