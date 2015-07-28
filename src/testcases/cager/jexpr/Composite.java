4249fa1376eb701a50b11f392dae98770695f694
Ligia Nistor <ligia.nicoleta@gmail.com>
2015-07-27 22:12:06 -0400

    adding existential params to pack unpack annotations

--- src/testcases/cager/jexpr/Composite.java
+++ src/testcases/cager/jexpr/Composite.java
@@ -86,21 +86,21 @@ int rc2;
 int newc = 1;
 unpack(this#0.5 left(ol, c1)[op]);
 if (this.left != null) {
-	unpack(ol#0.5 count(c1)[ol1, or1, lc1, rc1]);	 
+	unpack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];	 
 	newc = newc + this.left.count;
-	pack(ol#0.5 count(c1)[ol1, or1, lc1, rc1]);
+	pack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];
 }
-pack(this#0.5 left(ol, c1)[op]);
+pack(this#0.5 left(ol, c1))[op];
 
-unpack(this#0.5 right(or, c2)[op]);	
+unpack(this#0.5 right(or, c2))[op];	
 if (this.right != null) {
-	unpack(or#0.5 count(c2)[ol2, or2, lc2, rc2]);
+	unpack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
 	newc = newc + this.right.count;
-	pack(or#0.5 count(c2)[ol2, or2, lc2, rc2]);
+	pack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
 }
-pack(this#0.5 right(or, c2)[op]);		
+pack(this#0.5 right(or, c2))[op];		
 this.count = newc; 
-pack(this#1.0 count(newc)[ol, or, c1, c2]);
+pack(this#1.0 count(newc))[ol, or, c1, c2];
 }
 
 //This version assumes this is the right child of opp.
@@ -135,21 +135,21 @@ int rrc;
 //this function.
 if (this.parent != null) {
 	splitFrac(opp#k parent(), 2);
-	unpack(opp#k/2 parent()[opp.parent, opp.count]);
+	unpack(opp#k/2 parent())[opp.parent, opp.count];
 	//We get opp#1/2 count(lccc) from unpacking opp in parent()
-	unpack(opp#0.5 count(opp.count)[oll, this, llc, lcc]);
+	unpack(opp#0.5 count(opp.count))[oll, this, llc, lcc];
 	if (this == right[parent[this]]) {
 		//The rule in the formal system should be that
 		//if we have two object propositions with different parameters
 		//that are both packed, then the parameters should be the same.
 		addFrac(opp#0.5 right(this, lcc), opp#0.5 right(orr, rrc));
 		//Explain why we need the full fraction!!!
-		unpack(opp#1.0 right(this, lcc)[opp.parent]);
+		unpack(opp#1.0 right(this, lcc))[opp.parent];
 		addFrac(unpacked(this#0.5 count(lcc)), this#0.5 count(lcc));
 		
 		this.updateCount()[lcc, ol, or, opp, lc, rc, opp.count];
-		pack(this#k2 parent()[opp, lc + rc + 1]);
-		pack(opp#1.0 right(this, lcc)[opp.parent]);
+		pack(this#k2 parent())[opp, lc + rc + 1];
+		pack(opp#1.0 right(this, lcc))[opp.parent];
 			
 		this.parent.updateCountRec()[opp.parent, opp.count, opp.left, this, opp.left.count, lc + rc + 1];	
 	} else if (this == left[parent[this]]){
@@ -159,8 +159,8 @@ if (this.parent != null) {
 		addFrac(unpacked(this#0.5 count(lcc)), this#0.5 count(lcc));
 		
 		this.updateCount()[lcc, ol, or, opp, lc, rc, opp.count];
-		pack(this#k2 parent()[opp, lc + rc + 1]);
-		pack(opp#1.0 left(this, lcc)[opp.parent]);
+		pack(this#k2 parent())[opp, lc + rc + 1];
+		pack(opp#1.0 left(this, lcc))[opp.parent];
 			
 		this.parent.updateCountRec()[opp.parent, opp.count, this, opp.right, opp.left.count, lc + rc + 1];
 
@@ -170,22 +170,16 @@ if (this.parent != null) {
 	//After this addFrac, we know that this is unpacked in count()
 	 this.updateCount()[lcc, ol, or, opp, lc, rc, opp.count];
 	splitFrac(unpacked(this#1.0 count(lcc)), 2);
-	pack(this#k2 parent()[this.parent, lc + rc + 1]);
+	pack(this#k2 parent())[this.parent, lc + rc + 1];
 	}
 }
 
-
 void setLeft(Composite l) 
 ~ double k1, double k2, double k:
-requires (this != l);
-// TODO Might need to add !=null for other objects too
-requires (l != null);
-requires this.right != this.parent;
-requires l != this.parent;
-requires this != this.right;
-requires this#k1 parent();
-requires unpacked (l#k2 parent());	
-ensures (this#k parent());
+requires (this != l) && (l != null) && (this.right != this.parent) &&
+		(l != this.parent) && (this != this.right) && (this#k1 parent()) &&
+		unpacked (l#k2 parent())	
+ensures (this#k parent())
 {
 // Existentially quantified variable for UnpackParent(this,lcc)
 int lcc;
@@ -195,15 +189,15 @@ Composite or;
 
 if (l.parent==null) {	
 	l.parent = this;
-	unpack(this#k1 parent()[this.parent, lcc]);
-	unpack(this#0.5 count(lcc)[null, or, 0, this.right.count]);
+	unpack(this#k1 parent())[this.parent, lcc];
+	unpack(this#0.5 count(lcc))[null, or, 0, this.right.count];
 	addFrac(this#0.5 left(null, 0), this#0.5 left(null, 0));
-	unpack(this#1.0 left(null, 0)[this.parent]);	
+	unpack(this#1.0 left(null, 0))[this.parent];	
 	this.left = l;
-	this.left.count := l.left.count;
-	pack(this#1.0 left(l, l.left.count)[this.parent]);
+	this.left.count = l.left.count;
+	pack(this#1.0 left(l, l.left.count))[this.parent];
 	splitFrac(this#1.0 left(l, l.left.count));
-	pack(l#k2 parent()[l.parent, l.left.count]);	
+	pack(l#k2 parent())[l.parent, l.left.count];	
 	this.updateCountRec()[this.parent, lcc, l, this.right, l.left.count, this.right.count]; 	
 	}
 }
