efd8538f61934542dabcfd02c379aa28f824be0a
Ligia Nistor <ligia.nicoleta@gmail.com>
2015-11-30 20:48:48 -0500

    removed fracLocal, only left frac in. I don't need fracLocal

--- src/testcases/cager/jexpr/CompositeWithFracLocal.java
+++ src/testcases/cager/jexpr/CompositeWithFracLocal.java
@@ -72,51 +72,39 @@ int rc2;
 
 int newc; 
 
-//fracLocal should actually go in the translation
-// These could also be fracLocalCount[this] := fracCount[this];
-fracLocalCount[this] := 1.0; // fracLocalCount[this] :=  fracCount[this];
-fracLocalLeft[this] := 0.5; // fracLocalLeft[this] :=  fracLeft[this];
-fracLocalRight[this] := 0.5; // fracLocalRight[this] :=  fracRight[this];
-// maybe we don't need the op!=null part, maybe this always
-// takes place
-(op!=null) ~=> fracLocalCount[op] := fracCount[op];
-
 newc = 1;
 unpack(this#0.5 left(ol, c1))[op];
+(ol != null ~=> (fracCount[ol] := fracCount[ol] + 0.5);
 if (this.left != null) {
 	unpack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];	
-	fracLocalLeft[ol] := fracLocalLeft[ol] + 0.5; 
-	fracLocalRight[ol] := fracLocalRight[ol] + 0.5; 
+	fracLeft[ol] := fracLeft[ol] + 0.5; 
+	fracRight[ol] := fracRight[ol] + 0.5; 
 	newc = newc + this.left.count;
 	pack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];
-	fracLocalLeft[ol] := fracLocalLeft[ol] - 0.5;
-	fracLocalRight[ol] := fracLocalRight[ol] - 0.5;
+	fracLeft[ol] := fracLeft[ol] - 0.5;
+	fracRight[ol] := fracRight[ol] - 0.5;
 }
 pack(this#0.5 left(ol, c1))[op];
-(ol!=null) ~=> fracLocalCount[ol] := fracLocalCount[ol] - 0.5;
+(ol!=null) ~=> fracCount[ol] := fracCount[ol] - 0.5;
 
 unpack(this#0.5 right(or, c2))[op];	
-// Maybe set to 0 all the fracLocals that are not explicitly mentioned
-// in the pre-conditions.
-(or!=null) ~=> fracLocalCount[or] := fracLocalCount[or] + 0.5;
+(or!=null) ~=> fracCount[or] := fracCount[or] + 0.5;
 
 if (this.right != null) {
 	unpack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
-	fracLocalLeft[or] := fracLocalLeft[or] + 0.5; 
-	fracLocalRight[or] := fracLocalRight[or] + 0.5; 
+	fracLeft[or] := fracLeft[or] + 0.5; 
+	fracRight[or] := fracRight[or] + 0.5; 
 	newc = newc + this.right.count;
 	pack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
-	fracLocalLeft[or] := fracLocalLeft[or] - 0.5;
-	fracLocalRight[or] := fracLocalRight[or] - 0.5;
+	fracLeft[or] := fracLeft[or] - 0.5;
+	fracRight[or] := fracRight[or] - 0.5;
 }
 pack(this#0.5 right(or, c2))[op];
-(or != null) ~=> (fracLocalCount[or] := fracLocalCount[or] - 0.5);
+(or != null) ~=> (fracCount[or] := fracCount[or] - 0.5);
 this.count = newc; 
 pack(this#1.0 count(newc))[ol, or, c1, c2];
-fracLocalLeft[this] := fracLocalLeft[this] - 0.5;
-fracLocalRight[this] := fracLocalRight[this] - 0.5;
-// At this point I need to modify somehow the frac's also, 
-// not only the fracLocal's.
+fracLeft[this] := fracLeft[this] - 0.5;
+fracRight[this] := fracRight[this] - 0.5;
 }
 
 //This version assumes this is the right child of opp.
@@ -148,42 +136,18 @@ Composite orr;
 int llc;
 int rrc;
 
-// The assignments below go only in the translation!
-// Maybe set to 0 all the frac's that are mentioned in the 
-// precondition and in a second step add to them the actual fraction.
-// In this example we have fracCount mentioned twice in the pre-condition,
-// so the second time we need to add to the existing fraction.
-
-// First set all the frac's that are mentioned in the pre-condition to 0.
-fracLocalParent[this] := 0.0;
-fracLocalParent[opp] := 0.0
-fracLocalLeft[opp] := 0.0;
-fracLocalRight[opp] := 0.0;
-
-
-fracLocalParent[this] := fracLocalParent[this] + fracParent[this];
-(opp != null)  ~=> (fracLocalParent[opp] := fracLocalParent[opp] + fracParent[opp]); // Should this be := k?
-(opp != null)  ~=> (fracLocalLeft[opp] := fracLocalLeft[opp] + 0.5);
-(opp != null)  ~=> (fracLocalRight[opp] := fracLocalRight[opp] + 0.5);
-
-(opp == null) ~=> (fracLocalCount[this] := fracLocalCount[this] + 0.5);
-
-fracLocalCount[this] :=  fracLocalCount[this] + 0.5;
-
 
 //We already have access to this.parent from the precondition of 
 //this function.
 if (this.parent != null) {
-	splitFrac(opp#k parent(), 2);
-	fracParent[opp] := fracParent[opp] / 2.0;
-	// fracLocalParent should hold whatever is left from fracParent after some of it is consumed.
-	fracLocalParent[opp] := fracLocalParent[opp] - (fracParent[opp] - fracParent[opp] / 2.0);
-	
-	// Maybe for unpack, we have to consume the k/2 to get what we get from the ensures of
-	// the unpack.
+	// Even though an object proposition is unpacked, we still hold the
+	// fraction to it as before.
 	unpack(opp#k/2 parent())[opp.parent, opp.count];
 	//We get opp#1/2 count(lccc) from unpacking opp in parent()
+	fracCount[opp] := fracCount[opp] +0.5;
+	
 	unpack(opp#0.5 count(opp.count))[oll, this, llc, lcc];
+	
 	if (this == this.parent.right) {
 		//The rule in the formal system should be that
 		//if we have two object propositions with different parameters
