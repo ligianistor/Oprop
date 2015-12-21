58a568967e956e30afba2c99c264b3d2d37e3448
Ligia Nistor <ligia.nicoleta@gmail.com>
2015-12-20 21:13:28 -0500

    remove fraction annotations after unpacking

--- src/testcases/cager/jexpr/CompositeWithFracLocal.java
+++ src/testcases/cager/jexpr/CompositeWithFracLocal.java
@@ -39,7 +39,9 @@ predicate parent() =
 	(((op != null) && (op.left == this)) ~=> op#0.5 left(this, c)) &&
 	(((op != null) && (op.right == this)) ~=> op#0.5 right(this, c)) &&
 	(op == null ~=> (this#0.5 count(c)))
-	
+// After calling unpack() we do not need annotations because in the translation 
+// the body of the unpack() procedure says "frac == old(frac) + 0.5", for 
+// example.
 		
 void updateCount() 
 ~double k:
@@ -78,8 +80,6 @@ unpack(this#0.5 left(ol, c1))[op];
 (ol != null) ~=> (fracCount[ol] := fracCount[ol] + 0.5);
 if (this.left != null) {
 	unpack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];	
-	fracLeft[ol] := fracLeft[ol] + 0.5; 
-	fracRight[ol] := fracRight[ol] + 0.5; 
 	newc = newc + this.left.count;
 	pack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];
 	fracLeft[ol] := fracLeft[ol] - 0.5;
@@ -89,12 +89,9 @@ pack(this#0.5 left(ol, c1))[op];
 (ol!=null) ~=> fracCount[ol] := fracCount[ol] - 0.5;
 
 unpack(this#0.5 right(or, c2))[op];	
-(or!=null) ~=> fracCount[or] := fracCount[or] + 0.5;
 
 if (this.right != null) {
 	unpack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
-	fracLeft[or] := fracLeft[or] + 0.5; 
-	fracRight[or] := fracRight[or] + 0.5; 
 	newc = newc + this.right.count;
 	pack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
 	fracLeft[or] := fracLeft[or] - 0.5;
@@ -111,6 +108,8 @@ fracRight[this] := fracRight[this] - 0.5;
 void updateCountRec() 
 // TODO 
 // Shouldn't we say that k>0 here for all such k??
+// Or assume that whenever we see k, it actually means a predefined very
+// small value like 0.0001.
 ~ double k1, double k, double k2:
 Composite opp, int lcc,
 Composite or, Composite ol,
@@ -147,15 +146,8 @@ if (this.parent != null) {
 	// fraction to it as before.
 	unpack(opp#k/2 parent())[opp.parent, opp.count];
 	//We get opp#1/2 count(lccc) from unpacking opp in parent()
-	fracCount[opp] := fracCount[opp] +0.5;
-	(opp.parent != null) ~=> (fracParent[opp.parent] := fracParent[opp.parent] + k);
-	((opp.parent != null) && (opp.parent.left == opp)) ~=> (fracLeft[opp.parent] := fracLeft[opp.parent] + 0.5);
-	((opp.parent != null) && (opp.parent.right == opp)) ~=> (fracRight[opp.parent] := fracRight[opp.parent] + 0.5);
-	(opp.parent == null) ~=> (fracCount[opp]:=fracCount[opp] + 0.5);
 	
 	unpack(opp#0.5 count(opp.count))[oll, this, llc, lcc];
-	fracLeft[opp] := fracLeft[opp] + 0.5;
-	fracRight[opp] := fracRight[opp] + 0.5;
 			
 	if (this == this.parent.right) {
 		//The rule in the formal system should be that
@@ -165,7 +157,6 @@ if (this.parent != null) {
 		fracRight[opp] := 0.5 + 0.5;
 		//Explain why we need the full fraction!!!
 		unpack(opp#1.0 right(this, lcc))[opp.parent];
-		(this!=null) ~=> fracCount[this] := fracCount[this] + 0.5;
 		
 		addFrac(unpacked(this#0.5 count(lcc)), this#0.5 count(lcc));
 		fracCount[this] := 0.5 + 0.5;
@@ -215,7 +206,6 @@ if (this.parent != null) {
 		fracLeft[opp] := 0.5 + 0.5;
 		//Explain why we need the full fraction!!!
 		unpack(opp#1.0 left(this, lcc))[opp.parent];
-		(this!=null) ~=> fracCount[this] := fracCount[this] + 0.5;
 		
 		addFrac(unpacked(this#0.5 count(lcc)), this#0.5 count(lcc));
 		fracCount[this] := 0.5 + 0.5;
@@ -309,30 +299,12 @@ Composite or;
 if (l.parent==null) {	
 	l.parent = this;
 	unpack(this#k1 parent())[this.parent, lcc];
-	
-	fracCount[this] := fracCount[this] + 0.5;
-	if (this.parent!=null) { 
-		fracParent[this.parent] := fracParent[this.parent] + k; 
-	}
-	if ((this.parent != null) && (left[this.parent] == this)) {
-		fracLeft[this.parent] := fracLeft[this.parent] + 0.5;
-	}
-	if ((this.parent != null) && (right[this.parent] == this)) {
-		fracRight[this.parent] := fracRight[this.parent] + 0.5;
-	}
-	if (this.parent == null) {
-		fracCount[this] := fracCount[this] + 0.5;
-	}
-	
 	unpack(this#0.5 count(lcc))[null, or, 0, this.right.count];
-	fracLeft[this] := fracLeft[this] + 0.5;
-	fracRight[this] := fracRight[this] + 0.5;
 
 	addFrac(this#0.5 left(null, 0), this#0.5 left(null, 0));
 	fracLeft[this] := 0.5 + 0.5;
 	
 	unpack(this#1.0 left(null, 0))[this.parent];	
-	if (null!=null) { fracCount[null] := fracCount[null] + 0.5;
 	
 	this.left = l;
 	this.left.count = l.left.count;
@@ -362,7 +334,7 @@ if (l.parent==null) {
 	}
 	
 	this.updateCountRec()[this.parent, lcc, l, this.right, l.left.count, this.right.count]; 
-
+// Whenever we see k, we could replace it with 0.00001
 	if (this.parent != null) { fracParent[this.parent] := fracParent[this.parent] - k; } 
 	if ((this.parent != null) && (this.parent.left == this)) {
 		fracLeft[this.parent] := fracLeft[this.parent] - 0.5;
