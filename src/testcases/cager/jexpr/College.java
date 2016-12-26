cad649187d2e9e59f74159e019cc68f805694f4a
Ligia Nistor <ligia.nicoleta@gmail.com>
2016-12-26 16:52:08 -0500

    minor changes

--- src/testcases/cager/jexpr/College.java
+++ src/testcases/cager/jexpr/College.java
@@ -7,6 +7,11 @@ int endowment; // also intrinsic state that needs to be stored for each college
 
 predicate CollegeNumberField() = 
 	exists c:int :: this.collegeNumber -> c 
+	
+predicate CollegeFields() = 
+	exists c:int, e:int :: 
+	this.collegeNumber -> c && 
+	this.endowment -> e
 
 predicate collegeFacilitiesMany(int num) = 
 	exists c:int::
@@ -17,10 +22,10 @@ predicate collegeFacilitiesFew(int num) = exists c:int ::
 	(this.collegeNumber -> c) && (num <= 4 * c) 
 
 College(int number) 
-ensures this#1.0 CollegeNumberField()[number]
+ensures this#1.0 CollegeFields()[number, (number*1000)-5]
 {
 	this.collegeNumber = number;
-	this.endowment = (this.collegeNumber *1000) - 5;
+	this.endowment = (number *1000) - 5;
 }
 
 int getCollegeNumber() 
@@ -31,22 +36,22 @@ ensures this#1.0 CollegeNumberField()[result]
 
 // the method that calculates the extrinsic state
 IntCell getNumberFacilities(int campusNumber) 
-requires this#1 CollegeNumberField(this.collegeNumber)
-ensures result#1 MultipleOf(this.collegeNumber)
+requires this#1.0 CollegeNumberField(this.collegeNumber)
+ensures result#1.0 MultipleOf(this.collegeNumber)
 {
 	return new IntCell(this.collegeNumber * campusNumber);
 }
 
 boolean checkFewFacilities(int num) 
-requires this#1 collegeFacilitiesFew(num)
-ensures this#1 collegeFacilitiesFew(num)
+requires this#1.0 collegeFacilitiesFew(num)
+ensures this#1.0 collegeFacilitiesFew(num)
 {
 return (num <= 4 * this.collegeNumber);
 }
 
 boolean checkManyFacilities(int num) 
-requires this#1 collegeFacilitiesMany(num)
-ensures this#1 collegeFacilitiesMany(num)
+requires this#1.0 collegeFacilitiesMany(num)
+ensures this#1.0 collegeFacilitiesMany(num)
 {
 return (num >= 10 * this.collegeNumber);
 }
