77016c57778db9af2399208336cd0d2b409af125
ligianistor <ligia.nicoleta@gmail.com>
2017-04-13 14:13:32 -0400

    small changes to Composite example

--- src/testcases/cager/jexpr/Composite.java
+++ src/testcases/cager/jexpr/Composite.java
@@ -30,9 +30,6 @@ predicate count(int c) =
 		&& (this#0.5 left(ol, lc)) 
 		&& (this#0.5 right(or, rc)) 
 		
-// Surround op in FractionManipulationStatement by $op$
-// where op can be identifier or keyword, etc.
-//We do need to surround the fraction annotations by the respective if.
 predicate parent() =
 	exists Composite op, int c, double k:
 	this.parent -> op &&
@@ -43,7 +40,6 @@ predicate parent() =
 	(((op != null) && (op.right == this)) ~=> op#0.5 right(this, c)) &&
 	(op == null ~=> (this#0.5 count(c)))
 	
-// Composite() {}
 Composite(int c, Composite l, Composite r, Composite p) 
 ensures (this.count == c) && (this.left == l) && (this.right == r)
 		&& (this.parent == p)
@@ -53,24 +49,11 @@ ensures (this.count == c) && (this.left == l) && (this.right == r)
 	this.right = r;
 	this.parent = p;
 }
-	
-	// If in the ensures we have ==constant for a fraction
-	// I don't need to do frac=frac-constant1. if constant1 is mentioned in the
-	// pre-condition. So the equality in the postcondition overwrites whatever the
-	// precondition says.
-	// If in both the pre and postcondition we have k related to fractions
-	// then we can say frac=frac/2.0 and frac=frac*2.0 after the call to the 
-	// method because these statements do not contradict each other.
-	// The other case that I need to think of is if in the precondition we have 
-	// fraction k and in the postcondition we have a constant, and also the other way around.
+
 void updateCount() 
-	// It is implicit that double k, double k1, etc are > 0.
-	// TODO I need to make this explicit somehow.
 ~double k, double k1, double k2:
 int c, Composite ol, Composite or, Composite op,
 int c1, int c2, int c3:	
-	//Have to count the disjunctions and put the disjunction number in the fraction manipulation statement
-	//This way I can reconstruct which disjunctions are together.
 requires 
     ((op!=null) ~=> (unpacked(op#k1 left(op.left, op.left.count)) || 
     				unpacked(op#k2 right(op.right, op.right.count)))) &&
@@ -86,48 +69,28 @@ ensures (this#1.0 count(c1+c2+1)) &&
     (this#0.0 left(this.left, this.left.count)) && 
     (this#0.0 right(this.right, this.right.count))
 {
-// Existential variables for 
-// unpack(ol#0.5 count(c1)).
-// The programmer should put all the 
-// existential variables the he/she declares in the
-//beginning of the program.
 int newc;
-Composite ol1;
-Composite or1;
-int lc1;
-int rc1;
-
-// Existential variables for 
-// unpack(or#0.5 count(c2)).
-Composite ol2;
-Composite or2;
-int lc2;
-int rc2;
 
 newc = 1;
 unpack(this#0.5 left(ol, c1))[op];
 if (this.left != null) {
-	unpack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];	 
+	unpack(ol#0.5 count(c1))[ol.left, ol.right, ol.left.count, ol.right.count];	 
 	newc = newc + this.left.count;
-	pack(ol#0.5 count(c1))[ol1, or1, lc1, rc1];
+	pack(ol#0.5 count(c1))[ol.left, ol.right, ol.left.count, ol.right.count];
 }
 pack(this#0.5 left(ol, c1))[op];
 
 unpack(this#0.5 right(or, c2))[op];	
 if (this.right != null) {
-	unpack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
+	unpack(or#0.5 count(c2))[or.left, or.right, or.left.count, or.right.count];
 	newc = newc + this.right.count;
-	pack(or#0.5 count(c2))[ol2, or2, lc2, rc2];
+	pack(or#0.5 count(c2))[or.left, or.right, or.left.count, or.right.count];
 }
 pack(this#0.5 right(or, c2))[op];		
 this.count = newc; 
 pack(this#1.0 count(newc))[ol, or, c1, c2];
 }
 
-	// The programmer is required to put all the statements about a fracPred[obj] into a single one. ??
-	// TODO is that necessary?
-	// Like for unpacked(this# count) below.
-	// If we have a packed and an unpacked, the putting together of these is unpacked.
 void updateCountRec() 
 ~ double k1, double k, double k2, double k3:
 Composite opp, int lcc,
@@ -202,16 +165,11 @@ requires (this != l) && (l != null) && (this.left != this.parent) &&
 		(l#k2 parent())	
 ensures (this#k parent()) && (l#k3 parent())
 {
-// Existentially quantified variable for UnpackParent(this,lcc)
-int lcc;
-// Existentially quantified variables for UnpackCount(this,lcc)
-// The variable rc is also used in the call to updateCountRec()
-Composite or;
 unpack(l#k2 parent())[l.parent, l.count];
 if (l.parent==null) {	
 	l.parent = this;
-	unpack(this#k1 parent())[this.parent, lcc];
-	unpack(this#0.5 count(lcc))[null, or, 0, this.right.count];
+	unpack(this#k1 parent())[this.parent, this.count];
+	unpack(this#0.5 count(this.count))[null, this.right, 0, this.right.count];
 	addFrac(this#0.5 left(null, 0), this#0.5 left(null, 0));
 	unpack(this#1.0 left(null, 0))[this.parent];	
 	this.left = l;
@@ -219,7 +177,7 @@ if (l.parent==null) {
 	pack(this#1.0 left(l, l.left.count))[this.parent];
 	splitFrac(this#1.0 left(l, l.left.count));
 	pack(l#k2 parent())[l.parent, l.left.count];	
-	this.updateCountRec()[this.parent, lcc, l, this.right, l.left.count, this.right.count]; 	
+	this.updateCountRec()[this.parent, this.count, l, this.right, l.left.count, this.right.count]; 	
 } else {
 	pack(l#k2 parent())[l.parent, l.count];	
 }
